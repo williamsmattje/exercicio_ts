6eb97bae88ac8bdf23d50ec335ed0a965ddc8323
Williams Mattje <mattjewillians@gmail.com>
2024-04-11 14:49:36 -0300

    first commit

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(numero1, numero2) {
+    return numero1 * numero2;
+}
+const resultadoMultiplicacao = multiplicar(5, 10);
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+const mensagemSaudacao = saudacao("João");
---
+++ exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicar(numero1: number, numero2: number): number {
+  return numero1 * numero2;
+}
+
+const resultadoMultiplicacao = multiplicar(5, 10); 
+
+function saudacao(nome: string): string {
+  return `Olá ${nome}`;
+}
+
+const mensagemSaudacao = saudacao("João");
